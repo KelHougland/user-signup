672cac18d5a55d4ff5fdde7caa2d10e7a4c9721e
Kelly Hougland <knhougland@gmail.com>
2017-09-24 21:41:57 -0500

    Continuing 2.6 prep

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, redirect
+import cgi
 import os
 import jinja2
 
@@ -7,6 +9,20 @@ template_dir = os.path.join(os.path.dirname(__file__),
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(template_dir))
 
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
 def index():
     template = jinja_env.get_template('hello_form.html')
     return template.render()
+
+@app.route("/hello", methods=['post'])
+def hello():
+    first_name = request.form['first_name']
+    first_name = cgi.escape(first_name)
+    template = jinja_env.get_template('hello_greeting.html')
+    return "<h1>Hello, {0}!</h1>".format(first_name)
+
+
+app.run()
