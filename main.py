92f7cf66814f77eb117d9ef2df1e8662e9912a41
Kelly Hougland <knhougland@gmail.com>
2017-09-25 19:13:57 -0500

    trying to get errors by inputs

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+emadd=''
+user=''
 
 def invalid_email(address):
     if ("@" not in address) or ("." not in address):
@@ -24,7 +26,8 @@ def invalid_email(address):
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
-    return render_template('form.html',emadd = emadd, user = user, error=encoded_error and cgi.escape(encoded_error, quote=True))
+    errorloc = request.args.get('errorloc')
+    return render_template('form.html',emadd = emadd, user = user, errorloc = errorloc, error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
@@ -39,21 +42,25 @@ def sign_up():
     global emadd
     emadd = emailadd
 
-    if (not username) or (not password) or (not passconf):
-        error = "Please fill in this portion."
-        return redirect("/?error=" + error)
-
-    if (' ' in username) or (len(username)<3):
+    if (' ' in username) or (len(username)<3) or (len(username)>20) or (not username):
         error = "Username be at least 3 characters long and contain only alphanumeric characters and punctuation."
-        return redirect("/?error=" + error)
+        errorloc = 'a'
+        return redirect("/?error=" + error + '&' + '?errorloc='+errorloc)
+
+    if (' ' in password) or (len(password)<3) or (len(password)>20) or (not password):
+        error = "This is not a valid password."
+        errorloc = 'b'
+        return redirect("/?error=" + error + '&' + '?errorloc='+errorloc)
 
     if password != passconf:
         error = "Your passwords do not match."
-        return redirect("/?error=" + error)
+        errorloc = 'c'
+        return redirect("/?error=" + error + '&' + '?errorloc='+errorloc)
 
     if (len(emailadd)<3) or (len(emailadd)>20) or invalid_email(emailadd):
         error = "Please enter a valid email address."
-        return redirect("/?error=" + error)
+        errorloc = 'd'
+        return redirect("/?error=" + error + '&' + '?errorloc='+errorloc)
 
     return render_template('confirmation.html',username=username)
 
